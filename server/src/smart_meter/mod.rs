4ef44cdc24c0d900785513e6960ad4de2027449a
torfmaster <briefe@kebes.de>
2023-12-30 10:32:19 +0100

    test: facilitate local testing
     - add dummy configuration including loopback tty instructions
     - remove necessity for IR power supply, argument is now optional

--- server/src/smart_meter/mod.rs
+++ server/src/smart_meter/mod.rs
@@ -13,11 +13,13 @@ pub(crate) fn uart_ir_sensor_data_stream(config: &Configuration) -> impl AsyncRe
 }
 
 pub(crate) fn enable_ir_sensor_power_supply(config: &Configuration) {
-    let mut chip = Chip::new(&config.gpio_location).unwrap();
-    let output = chip.get_line(config.gpio_power_pin).unwrap();
-    let output_handle = output
-        .request(LineRequestFlags::OUTPUT, 0, "mirror-gpio")
-        .unwrap();
+    if let Some(gpio_location) = &config.gpio_location {
+        let mut chip = Chip::new(gpio_location).unwrap();
+        let output = chip.get_line(config.gpio_power_pin).unwrap();
+        let output_handle = output
+            .request(LineRequestFlags::OUTPUT, 0, "mirror-gpio")
+            .unwrap();
 
-    output_handle.set_value(1).unwrap();
+        output_handle.set_value(1).unwrap();
+    }
 }
